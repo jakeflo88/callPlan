6875a38be0a5c1d97ec082dcef95cad70787709e
jakeflo88 <jakeflorian.mcf@gmail.com>
2021-06-09 22:56:29 -0300

    Oh I guess I do remember how to use github after all

---
+++ callDirection.py
@@ -0,0 +1,42 @@
+# FOR DIRECTING CALL TRAFFIC
+import random
+
+
+# Call comes in
+call = ''
+
+# Good morning, Thistle Hyundai computer speaking, how can I direct your call?
+print('Good morning, Thistle Hyundai computer speaking, how can I direct your call?')
+
+call = input()
+
+
+print('Thank you, I will connect you to the ' + call + ' department now.\n')
+
+
+
+# Sales call
+if call ==  'sales':
+
+    print('*PA or just look*')
+
+# THIS IS WHAT IS HAPPENING NOW   
+#    print('Sales phone, "Ring ring Sales ring.."\n')
+#
+#    pickup = random.randint(0, 9)
+#
+#   if pickup >= 5:
+#       print('Hello! Would you like to buy a car?')
+#
+#   else:
+#       print('No answer.\n')
+
+
+# Service call
+
+#Parts
+
+# Direct call
+
+
+#
